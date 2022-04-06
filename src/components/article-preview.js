e30d1e87a6b0d9b4bcb2d84788b58cf783736b67
Ling K <ling.kan@outlook.com>
2022-04-07 00:26:19 +0100

    Fix article grid

--- src/components/article-preview.js
+++ src/components/article-preview.js
@@ -14,9 +14,9 @@ const ArticlePreview = ({ posts }) => {
       <ul className="grid grid-cols-2 gap-3">
         {posts.map((post) => {
           return (
-            <Link to={`/blog/${post.slug}`} key={post.slug} className="flex flex-col items-center bg-white rounded-lg border shadow-md md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+            <Link to={`/portfolio/${post.slug}`} key={post.slug} className="flex flex-col items-center bg-white rounded-lg border shadow-md md:grid md:grid-cols-3 md:max-w-xl md:max-h-lg hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
               <GatsbyImage alt="" className="object-cover w-full h-auto rounded-t-lg md:h-full md:w-full md:max-w-50 md:rounded-none md:rounded-l-lg" image={post.heroImage.gatsbyImageData} />
-              <div className="flex flex-col justify-between p-4 leading-normal">
+              <div className="flex flex-col justify-between p-4 leading-normal md:col-span-2">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{post.title}</h5>
                 <div className="mb-3 font-normal text-gray-700 dark:text-gray-400" dangerouslySetInnerHTML={{
                   __html: post.description.childMarkdownRemark.html,
