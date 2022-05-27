03081d6f61496eac918ef8103ad02fbe76af237d
Ling K <ling.kan@outlook.com>
2022-05-27 22:43:20 +0100

    Whitelist tailwind for styling in blog post

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx,json}",
+  ],
+  safelist: [
+    {
+      pattern: /col-(span)-(1|2|3|4|5|6)/,
+      variants: ['md'],
+    },
+    {
+      pattern: /grid-(cols|rows)-(1|2|3|4|5|6)/,
+      variants: ['md'],
+    },
   ],
   theme: {
     extend: {},
