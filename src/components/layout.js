672eaa601d74dde84a16a8f1b848893f41cf7da3
Ling K <ling.kan@outlook.com>
2022-05-28 17:21:46 +0100

    Add framer motion animation

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Seo from './seo'
 import Navigation from './navigation'
 import Footer from './footer'
+import { motion } from "framer-motion"
 
 class Template extends React.Component {
   render() {
@@ -11,7 +12,28 @@ class Template extends React.Component {
       <div >
         <Seo />
         <Navigation navList={navigation} socialList={socials} />
-        <main style={{ marginTop: mainHeaderSpacing }} role="main">{children}</main>
+        <motion.main
+          initial={{
+            opacity: 0,
+            x: -200
+          }}
+          animate={{
+            opacity: 1,
+            x: 0
+          }}
+          exit={{
+            opacity: 0,
+            x: 200
+          }}
+          transition={{
+            type: "spring",
+            mass: 0.5,
+            stiffness: 100,
+            duration: 0.1
+          }}
+        >
+          <main style={{ marginTop: mainHeaderSpacing }} role="main">{children}</main>
+        </motion.main>
         <Footer navList={navigation} socialList={socials} />
       </div>
     )
