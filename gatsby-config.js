6c3a2ee4f94ac9f3771a88c690ae5eec57bb3397
Ling K <ling.kan@outlook.com>
2022-06-06 22:45:37 +0100

    update ga config

--- gatsby-config.js
+++ gatsby-config.js
@@ -115,7 +115,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: process.env.GATSBY_GOOGLE_ANALYTICS_TRACKING_ID,
+        trackingIds: [process.env.GATSBY_GOOGLE_ANALYTICS_TRACKING_ID],
         pluginConfig: {
           head: true,
         },
