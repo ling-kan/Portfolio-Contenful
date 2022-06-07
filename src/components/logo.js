c8bf2eef18e4fa9e42b6a063623c102ade9ad4f5
Ling K <ling.kan@outlook.com>
2022-06-07 08:32:37 +0100

    Update dark colors

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
-const Logo = ({ darkMode }) =>
+const Logo = () =>
     <Link to="/" className="flex items-center">
         <span className={`self-center text-2xl text-black dark:text-white font-black whitespace-nowrap uppercase tracking-wide`}>Ling kan</span>
     </Link>
