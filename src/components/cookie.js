8c07db5bcb31c7c40dc7c03d237f56b4822924cd
Ling K <ling.kan@outlook.com>
2022-06-16 22:46:20 +0100

    Update cookie to small size

--- src/components/cookie.js
+++ src/components/cookie.js
@@ -47,26 +47,26 @@ const CookieConsent = () => {
     return (
         <>
             {!bannerHidden && (
-                <div class="fixed w-screen bottom-0 z-50">
-                    <div class="max-w-7xl m-3 p-3 sm:px-6 lg:px-8 bg-primary rounded-md">
-                        <div class="flex items-center justify-between flex-wrap">
-                            <div class="w-0 flex-1 flex items-center">
-                                <span class="flex p-2 rounded-lg bg-indigo-800">
-                                    <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                <div className="fixed w-screen bottom-0 z-50">
+                    <div className="max-w-sm m-3 p-3 py-4 bg-primary rounded-md">
+                        <div className="flex items-center justify-between flex-wrap">
+                            <div className="w-0 flex-1 flex items-center">
+                                <span className="flex p-2 rounded-lg">
+                                    <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
                                     </svg>
                                 </span>
-                                <p class="ml-3 text-xs text-white">
+                                <p className="ml-3 text-xs text-white">
                                     <span >This website uses cookies that help the website to function and also to track how you interact with our website. But for us to provide the best user experience, enable the specific cookies from Settings, and click on Accept.</span>
                                 </p>
                             </div>
-                            <div class="order-3 mt-4 mx-4 w-full flex justify-center md:block md:order-2 md:mt-auto  md:w-auto">
-                                <button onClick={EnableAnalytics} class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-black dark:bg-primary hover:bg-blue">Accept</button>
+                            <div className="order-3 mt-4 mx-4 w-full flex justify-center ">
+                                <button onClick={EnableAnalytics} className="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-black dark:bg-primary hover:bg-blue">Accept</button>
                             </div>
-                            <div class="order-2 flex-shrink-0 md:order-3 md:ml-3">
-                                <button type="button" onClick={() => setBannerHidden(true)} class="-mr-1 flex p-2 rounded-md hover:bg-blue focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
-                                    <span class="sr-only">Dismiss</span>
-                                    <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
+                            <div className="order-2 flex-shrink-0 ">
+                                <button type="button" onClick={() => { setBannerHidden(true) }} className="-mr-1 flex p-2 rounded-md hover:bg-blue focus:outline-none focus:ring-2 focus:ring-white sm:-mr-2">
+                                    <span className="sr-only">Dismiss</span>
+                                    <svg className="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true">
                                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
                                     </svg>
                                 </button>
