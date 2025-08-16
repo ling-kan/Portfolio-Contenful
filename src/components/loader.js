a5ecfffae1bb431185df7a54728b86a96796bbae
Ling K <ling.kan@outlook.com>
2025-08-16 13:27:57 +0100

    Update loader

--- src/components/loader.js
+++ src/components/loader.js
@@ -1,24 +1,31 @@
 import React from 'react'
-import animation from '../assets/lotties/DashboardReview.lottie.json';
-import Lottie from 'lottie-react';
+import CircularText from './motion/circular-text';
+// import animation from '../assets/lotties/DashboardReview.lottie.json';
+// import Lottie from 'lottie-react';
 
 const Loader = () => {
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: animation,
-        rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
-        }
-    };
+    // const defaultOptions = {
+    //     loop: true,
+    //     autoplay: true,
+    //     animationData: animation,
+    //     rendererSettings: {
+    //         preserveAspectRatio: "xMidYMid slice"
+    //     }
+    // };
     return (
         <div className='w-screen min-h-screen flex p-20'>
             <div className="m-auto h-fit">
                 <div className='max-w-xs max-h-xs'>
                     <div className='p-5'>
-                        <Lottie
+                        {/* <Lottie
                             options={defaultOptions}
                             alt="Loader"
+                        /> */}
+                        <CircularText
+                            text="LING KAN - PORTFOLIO - "
+                            onHover="speedUp"
+                            spinDuration={20}
+                            className="custom-class"
                         />
                     </div>
                     <div className="relative">
